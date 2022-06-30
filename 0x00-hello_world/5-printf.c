2060c537b5488bff6cfc27ffef2d309b32c109f1
Osiyoku Oluwatimilehin <osiyokuemmanuel@gmail.com>
2022-06-30 13:51:53 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#inlude <stdio.h>
+
+/**
+ * main - The program begins execution here
+ * Return: It gives an output of 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
