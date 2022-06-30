3482c4f82e94111751feb5f8d06d585aad800316
Osiyoku Oluwatimilehin <osiyokuemmanuel@gmail.com>
2022-06-30 13:13:59 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * C program to print a line
+ * we would use puts
+ * we won't use printf
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a mulitilingual puzzle");
+  return(0);
+}
