8d146c964b49b75b4b1e1eed11232411a3432199
Osiyoku Oluwatimilehin <osiyokuemmanuel@gmail.com>
2022-07-01 18:10:52 -0700

    Task 0 once again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,13 +4,13 @@
 
 /**
  *
- *  * main - assigns a random number to the value of n everytime
+ * main - assigns a random number to the value of n everytime
  *
- *   * it executes and prints it
+ * it executes and prints it
  *
- *    * Return: gives a 0 when successful
+ * Return: gives a 0 when successful
  *
- *    */
+ */
 
 int main(void)
 
@@ -24,15 +24,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 
- printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 
 else if (n < 0)
-	
- printf("%d is negative\n", n);
+
+printf("%d is negative\n", n);
 
 else if (n == 0)
 
- printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 
 return (0);
 
