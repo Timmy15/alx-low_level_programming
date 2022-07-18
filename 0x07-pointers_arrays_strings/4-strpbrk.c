920e8f5ffd20f7a967fdf0caecb3d00b36e7ba75
Osiyoku Oluwatimilehin <osiyokuemmanuel@gmail.com>
2022-07-18 09:57:36 -0700

    Task 4 again

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -21,8 +21,8 @@ char *_strpbrk(char *s, char *accept)
 		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (s[i] == accept[n])
-				return(s + i);
+				return (s + i);
 		}
 	}
-	return(NULL);
+	return (NULL);
 }
